c7e343dc83ea601907ca400f741fd93b0caaa0ac
rafa <rafaelsistem87@gmail.com>
2021-03-21 12:46:55 -0700

    REAC-NATIVE firebase expo

---
+++ screens/UserDetailScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const UserDetailScreen = () => {
+      return (
+            <View>
+                  <Text>
+                       UserDetailScreen
+                  </Text>
+            </View>
+      )
+}
+
+export default UserDetailScreen
---
+++ screens/UsersList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const UsersList = () => {
+      return (
+            <View>
+                  <Text>
+                          UserList
+                  </Text>
+            </View>
+      )
+}
+
+export default UsersList
